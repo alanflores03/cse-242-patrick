ac9b33909a36dd4f9b1d95104d64a10f7ef29e10
Ken Zhu <kez226@lehigh.edu>
2025-09-26 15:19:46 -0400

    working? find_none i think, i hope

--- code/blocks.py
+++ code/blocks.py
@@ -1,3 +1,4 @@
+import secrets
 from hashlib import sha256
 
 class Block:
@@ -10,9 +11,29 @@ class Block:
             self.hash_header = hash_header
             self.hash_root = hash_root
             self.timestamp = timestamp
-            self.diff_target = diff_target
+            self.diff_target = "7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
             self.nonce = nonce
-    
+
+
+
+def find_nonce(difficulty, root):
+    # turns root hex string to bytes
+    r = bytes.fromhex(root)
+    # turns difficulty hex string to an unsigned int
+    d = int.from_bytes(bytes.fromhex(difficulty), "big")
+
+    while True:
+        # gets a uniformly random number with 256 bits and converts to bytes
+        nonce = secrets.randbits(256)
+        nonce_bytes = nonce.to_bytes(32, "big")
+
+        # gets the raw byte hashed value of root (as bytes) plus nonce (as bytes)
+        holder = sha256(r + nonce_bytes).digest()
+
+        # if this value cast to an int is less than our target, return the nonce
+        if int.from_bytes(holder, "big") <= d:
+            return nonce
+        
 
 
 def hash_string(content):
