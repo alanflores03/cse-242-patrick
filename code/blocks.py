6c7c078260a7f756c8a10805d80e44f8ee80ceb9
alanflores03 <flores-alan@live.com>
2025-09-26 15:43:43 -0400

    Finished the validating header function

--- code/blocks.py
+++ code/blocks.py
@@ -18,3 +18,19 @@ class Block:
 def hash_string(content):
     hashed = hashlib.sha256(content.encode('utf-8')).hexdigest()
     return hashed
+
+
+# This function validates the block header data and ensures it is filled out correctly.
+# Returns true if valid, false if not. 
+def validate_header(header):
+    if len(header.hash_header) != 64:
+        return False
+    if  len(header.hash_root) != 64:
+        return False
+    if header.timestamp < 0:
+        return False
+    if header.diff_target <= 0:
+        return False
+    if not isinstance(header.nonce, int) or header.nonce < 0:
+        return False
+    return True
