31873c52bb330ca61d8512d967e3a00909769a91
Ken Zhu <kez226@lehigh.edu>
2025-09-26 14:38:14 -0400

    unfinished block class

--- code/blocks.py
+++ code/blocks.py
@@ -1,11 +1,14 @@
 
-class header:
-    def __init__(self, hash_header, hash_root, timestamp, diff_target, nonce):
-        self.hash_header = hash_header
-        self.hash_root = hash_root
-        self.timestamp = timestamp
-        self.diff_target = diff_target
-        self.nonce = nonce
+class Block:
+    def __init__(self):
+        self.ledger = None
+        self.header = None
 
-class block:
+    class Header:
+        def __init__(self, hash_header, hash_root, timestamp, diff_target, nonce):
+            self.hash_header = hash_header
+            self.hash_root = hash_root
+            self.timestamp = timestamp
+            self.diff_target = diff_target
+            self.nonce = nonce
     
